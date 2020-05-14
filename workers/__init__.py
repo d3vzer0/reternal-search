a32e1b8f53513585fc1b0eba7bcf56693c5daa2e
Joey Dreijer <jdreijer@schubergphilis.com>
2020-05-15 01:58:31 +0200

    Added worker to fetch sourcetypes

--- workers/__init__.py
+++ workers/__init__.py
@@ -4,20 +4,14 @@ import os
 import logging
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
-app = Celery('c2', backend=config['CELERY_BACKEND'], broker=config['CELERY_BACKEND'])
+app = Celery('search', backend=config['CELERY_BACKEND'], broker=config['CELERY_BACKEND'])
 app.conf.task_routes = {
     'c2.*': { 'queue': 'c2' },
-    'api.*': { 'queue': 'api' }    
+    'api.*': { 'queue': 'api' },
+    'search.*': { 'queue': 'search' }
 }
 
 app.autodiscover_tasks([
-    'workers.empire2',
+    'workers.splunk',
     'workers.system'
 ])
-
-app.conf.beat_schedule = {
-    'check-running-scenarios': {
-        'task': 'c2.system.scheduler',
-        'schedule': config['CELERY_TIMER'],
-    }
-}
--- workers/splunk/tasks/query.py
+++ workers/splunk/tasks/query.py
@@ -1,11 +1,10 @@
 from workers.splunk.api.splunk import Splunk
 from workers import app
 
-@app.task(name='search.query.splunk.run')
-def get_agents(query: str, earliest_time: str, latest_time: str):
-    ''' Run splunk query '''
-    # Examples, ignore
-    query = 'search `sysmon` event_id=1 |stats earliest(_time) as first_seen, last(_time) as last_seen by host_name'
-    Splunk(app_context='ThreatHunting').search(query, '-900d')
-    print('test')
+@app.task(name='search.modules.splunk.sourcetypes')
+def get_sourcetypes(earliest_time: str, latest_time: str):
+    ''' Run splunk query to get sourcetypes'''
+    query = '| metadata type=sourcetypes index=*'
+    get_sourcetypes = Splunk(app_context='ThreatHunting').search(query, earliest_time)
+    return get_sourcetypes
 
