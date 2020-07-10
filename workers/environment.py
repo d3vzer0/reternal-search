7b8a3c47bd101060186526f8553bd85a4714f7c7
Joey Dreijer <jdreijer@schubergphilis.com>
2020-07-11 00:44:33 +0200

    Added seperated broker/backend for celery

--- workers/environment.py
+++ workers/environment.py
@@ -2,7 +2,7 @@ import os
 
 config = {
     'CELERY_BACKEND': os.getenv('RT_CELERY_BACKEND', 'redis://localhost:6379'),
-    'CELERY_BROKER': os.getenv('RT_CELERY_BACKEND', 'redis://localhost:6379'),
+    'CELERY_BROKER': os.getenv('RT_CELERY_BROKER', 'redis://localhost:6379'),
     'CELERY_TIMER': os.getenv('RT_CELERY_TIMER', 10.0),
     'SPLUNK_TOKEN': os.getenv('RT_SPLUNK_TOKEN'),
     'SPLUNK_HOST': os.getenv('RT_SPLUNK_HOST', 'localhost'),
